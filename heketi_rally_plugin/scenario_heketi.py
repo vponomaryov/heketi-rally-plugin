524e07ca93bb6300474c6da27c0a8ed4fce302c4
Valerii Ponomarov <kiparis.kh@gmail.com>
2018-05-15 19:58:46 +0530

    Add possibility to set name prefix for heketi volumes

--- heketi_rally_plugin/scenario_heketi.py
+++ heketi_rally_plugin/scenario_heketi.py
@@ -3,20 +3,37 @@ import time
 from rally.task import scenario
 
 from heketi_rally_plugin import scenario_heketi_base as scenario_base
+from heketi_rally_plugin import utils
+
+
+def _get_volume_options(size=1, name_prefix="rally"):
+    volume_options = {}
+    if name_prefix:
+        if name_prefix[-1] != '-':
+            name_prefix += "-"
+        name_prefix.replace('_', '-')
+        name = "%s%s" % (name_prefix, utils.get_random_str(14))
+        volume_options["name"] = name
+    volume_options["size"] = int(size)
+    return volume_options
 
 
 @scenario.configure(name="Heketi.volume_create_delete")
 class HeketiVolumeCreateDeleteScenario(scenario_base.HeketiScenarioBase):
-    def run(self, size=1, volume_type='file', sleep_before_deletion=0):
+    def run(self, size=1, volume_type='file',
+            name_prefix="rally", sleep_before_deletion=0):
         """File/Block volume creation and deletion scenario.
 
         :param size: int, size of a volume in GiB
         :param volume_type: str, choices are 'file' and 'block'. Defines
             the type of a volume to be created.
+        :param name_prefix: str, custom string to be used as prefix
+            for volume names.
         :param sleep_before_deletion: float/int, time in seconds to sleep
             after volume creation and deletion.
         """
-        vol = self._volume_create({"size": int(size)}, volume_type=volume_type)
+        volume_options = _get_volume_options(size, name_prefix)
+        vol = self._volume_create(volume_options, volume_type=volume_type)
         if sleep_before_deletion > 0:
             time.sleep(sleep_before_deletion)
         self._volume_delete(vol["id"], volume_type=volume_type)
@@ -25,6 +42,7 @@ class HeketiVolumeCreateDeleteScenario(scenario_base.HeketiScenarioBase):
 @scenario.configure(name="Heketi.volume_create_expand_delete")
 class HeketiVolumeCreateExpandDeleteScenario(scenario_base.HeketiScenarioBase):
     def run(self, size=1, expand_size=1, volume_type='file',
+            name_prefix="rally",
             sleep_before_entension=0, sleep_before_deletion=0):
         """File/Block volume creation, expansion and deletion scenario.
 
@@ -33,6 +51,8 @@ class HeketiVolumeCreateExpandDeleteScenario(scenario_base.HeketiScenarioBase):
             in GiB
         :param volume_type: str, choices are 'file' and 'block'. Defines
             the type of a volume to be created.
+        :param name_prefix: str, custom string to be used as prefix
+            for volume names.
         :param sleep_before_entension: float/int, time in seconds to sleep
             before volume expansion.
         :param sleep_before_deletion: float/int, time in seconds to sleep
@@ -45,7 +65,8 @@ class HeketiVolumeCreateExpandDeleteScenario(scenario_base.HeketiScenarioBase):
             raise NotImplementedError(
                 "'expand block volume' feature is absent yet in Heketi.")
 
-        vol = self._volume_create({"size": int(size)}, volume_type=volume_type)
+        volume_options = _get_volume_options(size, name_prefix)
+        vol = self._volume_create(volume_options, volume_type=volume_type)
         if sleep_before_entension > 0:
             time.sleep(sleep_before_entension)
         self._volume_expand(
@@ -58,18 +79,22 @@ class HeketiVolumeCreateExpandDeleteScenario(scenario_base.HeketiScenarioBase):
 
 @scenario.configure(name="Heketi.volume_create_get_delete")
 class HeketiVolumeCreateGetDeleteScenario(scenario_base.HeketiScenarioBase):
-    def run(self, size=1, volume_type='file', sleep_before_deletion=0):
+    def run(self, size=1, volume_type='file',
+            name_prefix="rally", sleep_before_deletion=0):
         """File/Block volume creation, getting and deletion scenario.
 
         :param size: int, size of a volume in GiB
         :param volume_type: str, choices are 'file' and 'block'. Defines
             the type of a volume to be created.
+        :param name_prefix: str, custom string to be used as prefix
+            for volume names.
         :param sleep_before_getting: float/int, time in seconds to sleep
             before getting a volume.
         :param sleep_before_deletion: float/int, time in seconds to sleep
             before volume deletion.
         """
-        vol = self._volume_create({"size": int(size)}, volume_type=volume_type)
+        volume_options = _get_volume_options(size, name_prefix)
+        vol = self._volume_create(volume_options, volume_type=volume_type)
         self._volume_info(vol["id"], volume_type=volume_type)
         if sleep_before_deletion > 0:
             time.sleep(sleep_before_deletion)
