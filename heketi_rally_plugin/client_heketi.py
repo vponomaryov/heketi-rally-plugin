5a91f990865d0f3032ccc31fd2cf3241d9bfbd68
Valerii Ponomarov <kiparis.kh@gmail.com>
2018-08-28 20:58:09 +0530

    Add possibility to log Heketi API calls
    To do it, just use "--debug" rally option. Example:
    $ rally --debug task start samples/foo.yaml
    
    It this case following info will be logged:
    - HTTP URLs
    - HTTP headers
    - HTTP POST data
    - Return codes

--- heketi_rally_plugin/client_heketi.py
+++ heketi_rally_plugin/client_heketi.py
@@ -1,5 +1,38 @@
-import heketi
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
 import requests
+import sys
+
+import heketi
+from rally.common import logging
+
+
+http_client.HTTPConnection.debuglevel = 1
+LOG = logging.getLogger(__name__)
+
+
+class StdoutLogger(object):
+    def write(self, message):
+        if message.strip():
+            LOG.debug(message)
+
+    def flush(self):
+        return
+
+
+stdout_logger = StdoutLogger()
+
+
+def wrap_sys_stdout(f):
+    def wrapped_f(*args, **kwargs):
+        sys.stdout = stdout_logger
+        result = f(*args, **kwargs)
+        sys.stdout = sys.__stdout__
+        return result
+    return wrapped_f
 
 
 class HeketiClient(heketi.HeketiClient):
@@ -7,21 +40,45 @@ class HeketiClient(heketi.HeketiClient):
 
     bv_uri = '/blockvolumes'
 
+    @wrap_sys_stdout
+    def volume_create(self, *args, **kwargs):
+        return super(HeketiClient, self).volume_create(*args, **kwargs)
+
+    @wrap_sys_stdout
+    def volume_list(self, *args, **kwargs):
+        return super(HeketiClient, self).volume_list(*args, **kwargs)
+
+    @wrap_sys_stdout
+    def volume_info(self, *args, **kwargs):
+        return super(HeketiClient, self).volume_info(*args, **kwargs)
+
+    @wrap_sys_stdout
+    def volume_expand(self, *args, **kwargs):
+        return super(HeketiClient, self).volume_expand(*args, **kwargs)
+
+    @wrap_sys_stdout
+    def volume_delete(self, *args, **kwargs):
+        return super(HeketiClient, self).volume_delete(*args, **kwargs)
+
+    @wrap_sys_stdout
     def blockvolume_create(self, volume_options=None):
         req = self._make_request('POST', self.bv_uri, volume_options or {})
         if req.status_code == requests.codes.ok:
             return req.json()
 
+    @wrap_sys_stdout
     def blockvolume_list(self):
         req = self._make_request('GET', self.bv_uri)
         if req.status_code == requests.codes.ok:
             return req.json()
 
+    @wrap_sys_stdout
     def blockvolume_info(self, volume_id):
         req = self._make_request('GET', "%s/%s" % (self.bv_uri, volume_id))
         if req.status_code == requests.codes.ok:
             return req.json()
 
+    @wrap_sys_stdout
     def blockvolume_expand(self, volume_id, expand_size=None):
         # TODO(vponomar): remove raising of the following exception when
         #                 "expansion of block volume" feature is implemented
@@ -33,6 +90,7 @@ class HeketiClient(heketi.HeketiClient):
         if req.status_code == requests.codes.ok:
             return req.json()
 
+    @wrap_sys_stdout
     def blockvolume_delete(self, volume_id):
         req = self._make_request('DELETE', "%s/%s" % (self.bv_uri, volume_id))
         return req.status_code == requests.codes.NO_CONTENT
